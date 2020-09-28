1ed02ff6750f1500e072ff4428d0aa16e61ea779
DiegoCagnato <71875972+DiegoCagnato@users.noreply.github.com>
2020-09-28 16:57:42 +0200

    Add files via upload

---
+++ DadoMoneta/src/main/java/dadomoneta/DadoMoneta.java
@@ -0,0 +1,17 @@
+package dadomoneta;
+
+
+public class DadoMoneta {
+      
+	public static void main(String[] args){
+		
+		Moneta moneta = new Moneta();
+		
+		moneta.PrintLancio();
+		
+		Dado dado = new Dado();
+		
+		dado.PrintLancio();
+		
+	}
+}
